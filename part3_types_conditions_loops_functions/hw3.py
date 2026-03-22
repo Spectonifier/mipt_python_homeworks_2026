d2e682ad323e870efbeb3b27954ffd738663c416
Гридчин Михаил Владимирович <gridchin.mv@phystech.edu>
2026-03-22 19:11:01 +0300

    fixed Other categories

--- part3_types_conditions_loops_functions/hw3.py
+++ part3_types_conditions_loops_functions/hw3.py
@@ -24,6 +24,7 @@ EXPENSE_CATEGORIES = {
     "Clothing": ("Outerwear", "Casual", "Shoes", "Accessories"),
     "Education": ("Courses", "Books", "Tutors"),
     "Communications": ("Mobile", "Internet", "Subscriptions"),
+    "Other": ("SomeCategory", "SomeOtherCategory")
 }
 
 
